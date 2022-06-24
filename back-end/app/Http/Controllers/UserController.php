07d4b7b4de6b0f823a8beae5a88d9ac8ebc2903c
Nhan Vo Thanh <nhanvothanh719@gmail.com>
2022-06-24 22:32:34 +0700

    Get user data when login

---
+++ back-end/app/Http/Controllers/UserController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class UserController extends Controller
+{
+    public function getUser() {
+        return Auth::user();
+    }
+}
--- back-end/routes/api.php
+++ back-end/routes/api.php
@@ -4,11 +4,13 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\UserController;
 use App\Http\Controllers\PasswordResetController;
 
 Route::post('/login', [AuthController::class, 'login']);
 Route::post('/register', [AuthController::class, 'register']);
 Route::post('/forget-password', [PasswordResetController::class, 'sendEmailToResetPassword']);
-Route::post('/reset-password', [PasswordResetController::class, 'resetPassword']);
+Route::post('/reset-password', [PasswordResetController::class, 'resetPassword']);                  
+Route::get('/get-user', [UserController::class, 'getUser'])->middleware('auth:api'); //Get user data in case the user did login
 
 
