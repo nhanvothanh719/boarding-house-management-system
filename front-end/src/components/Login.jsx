48df7d59e6f37b17d0b70c4e2cdfaed2e2156785
Nhan Vo Thanh <nhanvothanh719@gmail.com>
2022-06-26 11:11:30 +0700

    Modify Login form

--- front-end/src/components/Login.jsx
+++ front-end/src/components/Login.jsx
@@ -10,17 +10,21 @@ class Login extends Component {
             <h1 class="h3 mb-3 font-weight-normal" style={{ "text-align": "center" }}>
               Sign in
             </h1>
-            <div class="social-login">
+            <div class="social-login row">
+              <div class="px-md-1">
               <button class="btn facebook-btn social-btn" type="button">
-                <span>
+                <span class="font-weight-bolder">
                   <i class="fab fa-facebook-f"></i> Sign in with Facebook
                 </span>{" "}
               </button>
+              </div>
+              <div>
               <button class="btn google-btn social-btn" type="button">
-                <span>
-                  <i class="fab fa-google-plus-g"></i> Sign in with Google+
+                <span class="font-weight-bolder">
+                  <i class="fab fa-google-plus-g"></i> Sign in with Google
                 </span>{" "}
               </button>
+              </div>
             </div>
             <p style={{ "text-align": "center" }}> OR </p>
             <input
