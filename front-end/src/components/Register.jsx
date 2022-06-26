eb554e73af1f5404a0646bcaddb884d6e0913d48
Nhan Vo Thanh <nhanvothanh719@gmail.com>
2022-06-26 21:29:55 +0700

    Register new user

--- front-end/src/components/Register.jsx
+++ front-end/src/components/Register.jsx
@@ -1,45 +1,99 @@
 import React, { Component } from "react";
-import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import axios from "axios";
+import { BrowserRouter as Router, Switch, Route, Link, Redirect } from "react-router-dom";
 
 class Register extends Component {
+  state = {
+    name: "",
+    email: "",
+    password: "",
+    password_confirmation: "",
+    message: "",
+  };
+
+  formSubmit = (e) => {
+    e.preventDefault();
+    const data = {
+      name: this.state.name,
+      email: this.state.email,
+      password: this.state.password,
+      password_confirmation: this.state.password_confirmation,
+    };
+
+    axios.post('/register', data)
+    .then((response) => {
+      localStorage.setItem('token', response.data.token);
+      this.setState({
+        isLogin: true,
+      })
+      this.props.setUser(response.data.user)
+    }).catch((error) => {
+      console.log(error);
+    });
+  }
+
   render() {
+    //Redirect to Login page if user registers successfully
+    if (this.state.isLogin) {
+      return <Redirect to={'/login'}/>
+    }
     return (
       <div class="row">
         <div class="jumbotron col-lg-4 offset-lg-4">
           <h3 class="text-center">Register</h3>
-          <form>
+          <form onSubmit={this.formSubmit}>
             <div class="form-group">
-              <label for="exampleInputEmail1">Username</label>
+              <label for="inputName">Full name</label>
               <input
                 type="text"
                 class="form-control"
-                id="exampleInputEmail1"
+                name="name"
+                id="inputName"
                 aria-describedby="emailHelp"
+                onChange={(e) => {
+                  this.setState({ name: e.target.value });
+                }}
+                required
               />
             </div>
             <div class="form-group">
-              <label for="exampleInputEmail1">Email address</label>
+              <label for="inputEmail">Email address</label>
               <input
                 type="email"
                 class="form-control"
-                id="exampleInputEmail1"
+                name="email"
+                id="inputEmail"
                 aria-describedby="emailHelp"
+                onChange={(e) => {
+                  this.setState({ email: e.target.value });
+                }}
+                required
               />
             </div>
             <div class="form-group">
-              <label for="exampleInputPassword1">Password</label>
+              <label for="inputPassword">Password</label>
               <input
                 type="password"
                 class="form-control"
-                id="exampleInputPassword1"
+                name="password"
+                id="inputPassword"
+                onChange={(e) => {
+                  this.setState({ password: e.target.value });
+                }}
+                required
               />
             </div>
             <div class="form-group">
               <label for="confirmedPassword">Confirmed password</label>
               <input
                 type="password"
+                name="password_confirmation"
                 class="form-control"
                 id="confirmedPassword"
+                onChange={(e) => {
+                  this.setState({ password_confirmation: e.target.value });
+                }}
+                required
               />
             </div>
             <button type="submit" class="btn btn-primary btn-block">
