9aa8a32272b94b07f663023b897362870fa3c50d
Nhan Vo Thanh <nhanvothanh719@gmail.com>
2022-07-08 11:37:55 +0700

    #51 Design sidebar

--- front-end/src/pages/Dashboard/Dashboard.jsx
+++ front-end/src/pages/Dashboard/Dashboard.jsx
@@ -1,11 +1,19 @@
-import React, { Component, Fragment } from 'react'
-import TopBar from '../../components/Dashboard/TopBar'
+import React, { Component, Fragment } from 'react';
+import SideBar from '../../components/Dashboard/SideBar';
+import TopBar from '../../components/Dashboard/TopBar';
+import { Container} from 'react-bootstrap';
+import "../../assets/css/Dashboard/common.css";
 
 export class Dashboard extends Component {
   render() {
     return (
       <Fragment>
-        <TopBar/>
+        <div className="home">
+        <SideBar/>
+        <div className='homeContainer'>
+          Container
+        </div>
+        </div>
       </Fragment>
     )
   }
