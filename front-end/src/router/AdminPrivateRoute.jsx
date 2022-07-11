8bdb0df332fa7075145912480b99ad1c54889014
Nhan Vo Thanh <nhanvothanh719@gmail.com>
2022-07-11 23:01:09 +0700

    #47 Update AdminPrivateRoute file

---
+++ front-end/src/router/AdminPrivateRoute.jsx
@@ -0,0 +1,55 @@
+import React, { useState, useEffect } from "react";
+import { Redirect, Route, useHistory } from "react-router-dom";
+import MasterLayout from "../pages/Dashboard/MasterLayout";
+import RestClient from "../RestAPI/RestClient";
+import AppUrl from "../RestAPI/AppUrl";
+import Loading from "../components/Loading";
+import axios from "axios";
+import swal from "sweetalert";
+
+function AdminPrivateRoute({ ...rest }) {
+  const history = useHistory();
+  
+  const [isAuthenticated, setAuthenticated] = useState(false);
+  const [isLoading, setLoading] = useState(true);
+
+  useEffect(() => {
+    RestClient.GetRequest(AppUrl.CheckAuthenticated).then((response) => {
+      if (response.status === 200) {
+        setAuthenticated(true);
+        setLoading(false);
+      }
+    });
+    return () => {
+      setAuthenticated(false);
+    };
+  }, []);
+
+  axios.interceptors.response.use(undefined, function axiosRetryInterceptors(error) {
+    if(error.response.status === 401) {
+      swal("Unauthenticated", error.response.data.message, "warning");
+      history.push("/home");
+    }
+    return Promise.reject(error);
+  });
+
+  if(isLoading) {
+    return <Loading />
+  }
+
+  return (
+    <Route
+      {...rest} //name, path,...
+      component={({ props, location }) =>
+        isAuthenticated ? (
+          <MasterLayout {...props} />
+        ) : (
+          <Redirect to={{ pathname: "/login", state: { from: location } }} />
+        )
+      }
+    />
+  );
+}
+
+
+export default AdminPrivateRoute;
