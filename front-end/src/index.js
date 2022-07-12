77c64b393896abca610f7dfde91580d7034d96f5
Nhan Vo Thanh <nhanvothanh719@gmail.com>
2022-07-12 11:29:55 +0700

    #47 Fix 401 unauthorized API error

--- front-end/src/index.js
+++ front-end/src/index.js
@@ -5,14 +5,13 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import axios from 'axios';
 
+
 //Set base URL
 axios.defaults.baseURL = 'http://127.0.0.1:8000/api';
 //Set Bearer token to route
 axios.defaults.headers.common['Authorization'] = 'Bearer ' + localStorage.getItem('token');
 //CSRF protection
-window.axios.defaults.headers.common = {
-  'X-Requested-With': 'XMLHttpRequest',
-};
+axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 
 ReactDOM.render(
   <React.StrictMode>
