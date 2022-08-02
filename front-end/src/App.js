5d130817c6b6c605ec8522865314b514bd7dc8aa
Nhan Vo Thanh <nhanvothanh719@gmail.com>
2022-08-03 00:33:32 +0700

    #68 Integrate tawk.to into web app

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -2,11 +2,15 @@ import React from 'react';
 import { BrowserRouter as Router } from 'react-router-dom';
 import ReactDOM from 'react-dom';
 import AppRouter from './router/AppRouter';
+import TawkMessengerReact from '@tawk.to/tawk-messenger-react';
 
 function App() {
   return (
     <Router>
       <AppRouter/>
+      <TawkMessengerReact
+                propertyId="62e95dc037898912e960e203"
+                widgetId="1g9fpce2n"/>
     </Router>
   );
 }
