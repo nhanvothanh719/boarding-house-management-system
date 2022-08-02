4932cccd3131cb116908cf6afd37821992bdb5bf
Nhan Vo Thanh <nhanvothanh719@gmail.com>
2022-08-03 00:56:18 +0700

    #68 Integrate messenger into web app

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,16 +1,22 @@
-import React from 'react';
-import { BrowserRouter as Router } from 'react-router-dom';
-import ReactDOM from 'react-dom';
-import AppRouter from './router/AppRouter';
-import TawkMessengerReact from '@tawk.to/tawk-messenger-react';
+import React from "react";
+import { BrowserRouter as Router } from "react-router-dom";
+import ReactDOM from "react-dom";
+import AppRouter from "./router/AppRouter";
+import TawkMessengerReact from "@tawk.to/tawk-messenger-react";
+import MessengerCustomerChat from "react-messenger-customer-chat";
 
 function App() {
   return (
     <Router>
-      <AppRouter/>
+      <AppRouter />
       <TawkMessengerReact
-                propertyId="62e95dc037898912e960e203"
-                widgetId="1g9fpce2n"/>
+        propertyId="62e95dc037898912e960e203"
+        widgetId="1g9fpce2n"
+      />
+      <MessengerCustomerChat
+        pageId="104103952405175"
+        appId="1160238107906300"
+      />
     </Router>
   );
 }
