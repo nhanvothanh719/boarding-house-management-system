fedad655503ac06e3bee55ec792e85087852c59d
Nhan Vo Thanh <nhanvothanh719@gmail.com>
2022-08-10 01:18:13 +0700

    #76 Add chart to display total breach make percentage

--- front-end/src/pages/Admin/Dashboard/Breach/BreachDetails.jsx
+++ front-end/src/pages/Admin/Dashboard/Breach/BreachDetails.jsx
@@ -1,8 +1,7 @@
-import React from 'react'
+import React, { PureComponent } from "react";
+import { Pie, PieChart, Sector } from "recharts";
 
 export default function BreachDetails({ match }) {
-    const breachId = match.params.breachID;
-  return (
-    <div>Breach Details {breachId}</div>
-  )
+  const breachId = match.params.breachID;
+  return <div>Breach Details {breachId}</div>;
 }
--- front-end/src/pages/Admin/Dashboard/Breach/BreachHistories.jsx
+++ front-end/src/pages/Admin/Dashboard/Breach/BreachHistories.jsx
@@ -16,7 +16,10 @@ import {
   Legend,
   Bar,
   BarChart,
-  LabelList
+  LabelList,
+  Pie,
+  PieChart,
+  Sector
 } from "recharts";
 
 import Loading from "../../../../components/Loading/Loading";
@@ -34,7 +37,8 @@ export default function BreachHistories() {
   const [breachHistoriesChange, setBreachHistoriesChange] = useState(false);
   const [breachesList, setBreachesList] = useState([]);
   const [violateMoment, setViolateMoment] = useState(moment());
-  const [barChartData, setBarChartData] = useState([]);
+  const [chartData, setChartData] = useState([]);
+  const [activeIndex, setActiveIndex] = useState(0);
 
   useEffect(() => {
     axios.get(AppUrl.ShowBreaches).then((response) => {
@@ -44,8 +48,7 @@ export default function BreachHistories() {
     });
     axios.get(AppUrl.GetTotalNumberBreachMade).then((response) => {
       if (response.data.status === 200) {
-        setBarChartData(response.data.breachTotals);
-        console.log(response.data.breachTotals);
+        setChartData(response.data.breachTotals);
       }
     });
     axios.get(AppUrl.ShowBreachHistories).then((response) => {
@@ -121,6 +124,57 @@ export default function BreachHistories() {
     }
   };
 
+  const renderActiveShape = (props) => {
+    const RADIAN = Math.PI / 180;
+    const {
+      cx, cy, midAngle, innerRadius, outerRadius, startAngle, endAngle,
+      fill, payload, percent, value,
+    } = props;
+    const sin = Math.sin(-RADIAN * midAngle);
+    const cos = Math.cos(-RADIAN * midAngle);
+    const sx = cx + (outerRadius + 10) * cos;
+    const sy = cy + (outerRadius + 10) * sin;
+    const mx = cx + (outerRadius + 30) * cos;
+    const my = cy + (outerRadius + 30) * sin;
+    const ex = mx + (cos >= 0 ? 1 : -1) * 22;
+    const ey = my;
+    const textAnchor = cos >= 0 ? 'start' : 'end';
+
+    return (
+      <g>
+        <text x={cx} y={cy} dy={8} textAnchor="middle" fill={fill}>{payload.name}</text>
+        <Sector
+          cx={cx}
+          cy={cy}
+          innerRadius={innerRadius}
+          outerRadius={outerRadius}
+          startAngle={startAngle}
+          endAngle={endAngle}
+          fill={fill}
+        />
+        <Sector
+          cx={cx}
+          cy={cy}
+          startAngle={startAngle}
+          endAngle={endAngle}
+          innerRadius={outerRadius + 6}
+          outerRadius={outerRadius + 10}
+          fill={fill}
+        />
+        <path d={`M${sx},${sy}L${mx},${my}L${ex},${ey}`} stroke={fill} fill="none" />
+        <circle cx={ex} cy={ey} r={2} fill={fill} stroke="none" />
+        <text x={ex + (cos >= 0 ? 1 : -1) * 12} y={ey} textAnchor={textAnchor} fill="#333">{`Total: ${value}`}</text>
+        <text x={ex + (cos >= 0 ? 1 : -1) * 12} y={ey} dy={18} textAnchor={textAnchor} fill="#999">
+          {`(${(percent * 100).toFixed(2)}%)`}
+        </text>
+      </g>
+    );
+  };
+
+  const onPieEnter = (data, index) => {
+		setActiveIndex(index);
+	};
+
   var columns = [];
   if (loading) {
     return <Loading />;
@@ -138,7 +192,6 @@ export default function BreachHistories() {
       {
         field: "violate_at",
         title: "Violate at",
-        type: "datetime",
       },
     ];
   }
@@ -148,8 +201,8 @@ export default function BreachHistories() {
       <BarChart 
       width={500} 
       height={300} 
-      data={barChartData}
-      margin={{ top: 30, right: 30, left: 20, bottom: 5 }}
+      data={chartData}
+      margin={{ top: 30, right: 30, left: 40, bottom: 5 }}
       >
         <CartesianGrid strokeDasharray="3 3" />
         <XAxis dataKey="name" />
@@ -160,6 +213,25 @@ export default function BreachHistories() {
         <LabelList dataKey="total" position="top" />
         </Bar>
       </BarChart>
+
+      <PieChart 
+      width={550} 
+      height={550}
+      margin={{ top: 10, right: 100, left: 10, bottom: 5 }}
+      >
+				<Pie
+					activeIndex={activeIndex}
+					activeShape={renderActiveShape}
+					data={chartData}
+					cx={200}
+					cy={200}
+					innerRadius={60}
+					outerRadius={80}
+					fill="#8884d8"
+					dataKey="total"
+					onMouseEnter={onPieEnter}
+				/>
+			</PieChart>
 
       <MaterialTable
         columns={columns}
